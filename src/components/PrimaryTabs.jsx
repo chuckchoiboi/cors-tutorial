898e06ccadae5ca054378f6d04901b209c92576a
chuckchoiboi <hyunchoi94@gmail.com>
2021-05-14 20:31:04 -0700

    add PrimaryTabs component

---
+++ src/components/PrimaryTabs.jsx
@@ -0,0 +1,11 @@
+const PrimaryTabs = () => {
+    return (
+        <div style={{ width: '40%'}}>
+            <ul className="nav nav-tabs">
+                
+            </ul>
+        </div>
+    )
+}
+
+export default PrimaryTabs
