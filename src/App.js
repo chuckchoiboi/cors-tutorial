2a1d9157559c6d3719ab1b9cc7db292a9d316766
chuckchoiboi <hyunchoi94@gmail.com>
2021-05-14 20:52:06 -0700

    add primaryTabs state and pass to PrimaryTabs component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+import { useState } from 'react'
 import './App.css';
-
-// components
 import PrimaryTabs from './components/PrimaryTabs'
 
 function App() {
 
-  const tabs = ['Simple Request', 'Preflight Request', 'Credential Request']
+  const [primarySelected, setPrimarySelected] = useState('Simple Request')
+  const primaryTabs = ['Simple Request', 'Preflight Request', 'Credential Request']
 
   return (
     <div className="d-flex justify-content-center flex-row mt-4">
-      <PrimaryTabs tabs={tabs} />
+      <PrimaryTabs tabs={primaryTabs} selected={primarySelected} setSelected={setPrimarySelected}/>
     </div>
   );
 }
