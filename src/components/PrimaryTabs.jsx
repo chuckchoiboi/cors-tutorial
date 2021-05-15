f7f9b28e7e79e33f78436f529cf090e2c7c08bbc
chuckchoiboi <hyunchoi94@gmail.com>
2021-05-14 20:38:34 -0700

    add tabs prop and iterate each elements of tabs

--- src/components/PrimaryTabs.jsx
+++ src/components/PrimaryTabs.jsx
@@ -1,8 +1,18 @@
-const PrimaryTabs = () => {
+const PrimaryTabs = ({tabs}) => {
     return (
         <div style={{ width: '40%'}}>
             <ul className="nav nav-tabs">
-                
+                {
+                    tabs.map( tab => {
+                        return (
+                            <li className="nav-item" key={ tab }>
+                                <a className="nav-link" href="/">
+                                    { tab }
+                                </a>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </div>
     )
