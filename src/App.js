323f2d1f026065e69c55e1b2a6e26f0064074d5b
chuckchoiboi <hyunchoi94@gmail.com>
2021-05-15 21:24:35 -0700

    make send request button use dynamic fetch request

--- src/App.js
+++ src/App.js
@@ -83,15 +83,15 @@ function App() {
     <div className="d-flex justify-content-center flex-row mt-4">
       <PrimaryTabs tabs={primaryTabs} selected={primarySelected} setSelected={setPrimarySelected} setSecondarySelected={setSecondarySelected}>
         <SecondaryTabs isSelected={ primarySelected === 0} tabs={secondaryTabs.simple} selected={secondarySelected} setSelected={setSecondarySelected} >
-          <Tab/>
+          <Tab isSelected={ secondarySelected === 0 } data={simpleData[secondarySelected]}/>
         </SecondaryTabs>
 
         <SecondaryTabs isSelected={ primarySelected === 1} tabs={secondaryTabs.preflight} selected={secondarySelected} setSelected={setSecondarySelected}>
-          <Tab/>
+          <Tab isSelected={ secondarySelected === 1 } data={preflightData[secondarySelected]}/>
         </SecondaryTabs>
 
         <SecondaryTabs isSelected={ primarySelected === 2} tabs={secondaryTabs.credentialed} selected={secondarySelected} setSelected={setSecondarySelected}>
-          <Tab/>
+          <Tab isSelected={ secondarySelected === 2 } data={credentialedData[secondarySelected]}/>
         </SecondaryTabs>
       </PrimaryTabs>
     </div>
--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,8 +1,18 @@
 const Tab = ({data}) => {
+
+    const sendReq = (data) => {
+        if (data.header) {
+            fetch(data.url, data.header)
+        } else {
+            fetch(data.url)
+        }
+    }
+
     return (
         <div className="Tab">
             <p>Server</p>
             <p>Request</p>
+            <button className="btn btn-primary" onClick={() => {sendReq(data)}}>Send Request</button>
             <p>Console</p>
             <p>Response</p>
         </div>
