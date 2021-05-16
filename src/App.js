97fcf26effc2fbe0a0d799698e032247c2182091
chuckchoiboi <hyunchoi94@gmail.com>
2021-05-15 21:14:03 -0700

    add fetch urls to the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css';
 import PrimaryTabs from './components/PrimaryTabs'
 import SecondaryTabs from './components/SecondaryTabs'
+import Tab from './components/Tab'
 
 function App() {
 
@@ -13,20 +14,84 @@ function App() {
     preflight: ['Error 1', 'Error 2', 'Error 3', 'Valid Condition'],
     credentialed: ['Error 1', 'Error 2', 'Valid Condition']
   }
+  const baseURL = 'https://cors-tutorial-server.herokuapp.com/api'
+
+  const simpleData = [
+    {
+      url: baseURL + '/simple/no-origin'
+    },
+    {
+      url: baseURL + '/simple/wildcard-origin'
+    },
+    {
+      url: baseURL + '/simple/bad-origin'
+    },
+    {
+      url: baseURL + '/simple/good-origin'
+    }
+  ]
+
+  const preflightData = [
+    {
+      url: baseURL + '/preflight/bad-origin',
+      header: {
+        method: 'DELETE'
+      }
+    },
+    {
+      url: baseURL + '/preflight/bad-method',
+      header: {
+        method: 'DELETE'
+      }
+    },
+    {
+      url: baseURL + '/preflight/req-bad-origin',
+      header: {
+        method: 'DELETE'
+      }
+    },
+    {
+      url: baseURL + '/preflight/good-origin',
+      header: {
+        method: 'DELETE'
+      }
+    }
+  ]
+
+  const credentialedData = [
+    {
+      url: baseURL + '/credentialed/wildcard-origin',
+      header: {
+        credentials: 'include'
+      }
+    },
+    {
+      url: baseURL + '/credentialed/good-origin',
+      header: {
+        credentials: 'include'
+      }
+    },
+    {
+      url: baseURL + '/credentialed/good-request',
+      header: {
+        credentials: 'include'
+      }
+    }
+  ]
 
   return (
     <div className="d-flex justify-content-center flex-row mt-4">
       <PrimaryTabs tabs={primaryTabs} selected={primarySelected} setSelected={setPrimarySelected} setSecondarySelected={setSecondarySelected}>
         <SecondaryTabs isSelected={ primarySelected === 0} tabs={secondaryTabs.simple} selected={secondarySelected} setSelected={setSecondarySelected} >
-          Hello1
+          <Tab/>
         </SecondaryTabs>
 
         <SecondaryTabs isSelected={ primarySelected === 1} tabs={secondaryTabs.preflight} selected={secondarySelected} setSelected={setSecondarySelected}>
-          Hello2
+          <Tab/>
         </SecondaryTabs>
 
         <SecondaryTabs isSelected={ primarySelected === 2} tabs={secondaryTabs.credentialed} selected={secondarySelected} setSelected={setSecondarySelected}>
-          Hello3
+          <Tab/>
         </SecondaryTabs>
       </PrimaryTabs>
     </div>
---
+++ src/components/Tab.jsx
@@ -0,0 +1,12 @@
+const Tab = ({data}) => {
+    return (
+        <div className="Tab">
+            <p>Server</p>
+            <p>Request</p>
+            <p>Console</p>
+            <p>Response</p>
+        </div>
+    )
+}
+
+export default Tab
