3bbcf7e469632669514ebce1a5cb2195afd2a75c
chuckchoiboi <hyunchoi94@gmail.com>
2021-05-17 23:12:19 -0700

    adjust font size

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
@@ -9,15 +10,17 @@ const Tab = ({data}) => {
     res.status(200).json({ title: 'Hello World!' })
 })`;
 
-    const request = data.header ? `fetch('${data.url}', '${data.header}')` : `fetch('${data.url}')`
+    const request = data.header ? `fetch('${data.url}', '${data.header}')` : `fetch('${data.url}')`;
 
-    const console = "Access to fetch at 'https://cors-tutorial-server.herokuapp.com/api/simple/no-origin' from origin 'https://www.chuckchoi.me' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled."
+    const console = "Access to fetch at 'https://cors-tutorial-server.herokuapp.com/api/simple/no-origin' from origin 'https://www.chuckchoi.me' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.";
+
+    const [response, setResponse] = useState('')
 
     const sendReq = (data) => {
         if (data.header) {
-            fetch(data.url, data.header)
+            fetch(data.url, data.header).then(res => res.json()).then(data => setResponse(`${data}`) )
         } else {
-            fetch(data.url)
+            fetch(data.url).then(res => res.json()).then(data => setResponse(`${data}`))
         }
     }
 
@@ -38,7 +41,7 @@ const Tab = ({data}) => {
                 </SyntaxHighlighter>
             <p>Response</p>
                 <SyntaxHighlighter language="javascript" style={a11yDark} lineProps={{style: {wordBreak: 'break-all', whiteSpace: 'pre-wrap'}}} wrapLines={true}>
-                    {`${request}`}
+                    {`${response}`}
                 </SyntaxHighlighter>
         </div>
     )
